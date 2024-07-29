494a784b535cdd61e5c2dd98f3cdc4fba86970f4
THOMAS LOUIS <tomlui2010@gmail.com>
2024-07-29 16:48:39 +1000

    updated docker commands

---
+++ backend/run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker image build -t tomlui2010/pawsclaws-backend-app:latest .
+
+# Step 2: 
+# List docker images
+docker images | grep "pawsclaws"
+
+# Step 3: 
+# Run flask app
+docker run -d -p 80:5000 tomlui2010/pawsclaws-backend-app:latest
