5101a78340cd2e1451ffe623a22416a5be896850
THOMAS LOUIS <tomlui2010@gmail.com>
2024-07-30 15:26:57 +1000

    config changes

--- backend/run_docker.sh
+++ backend/run_docker.sh
@@ -12,4 +12,4 @@ docker images | grep "pawsclaws"
 
 # Step 3: 
 # Run flask app
-docker run -d -p 80:5000 tomlui2010/pawsclaws-backend-app:latest
+docker run -d -p 5001:5000 tomlui2010/pawsclaws-backend-app:latest
