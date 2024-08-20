fc50f7ab5fdbc6cc5fdf2030853e587a1a8ae838
THOMAS LOUIS <tomlui2010@gmail.com>
2024-08-20 18:12:30 +1000

    Added job to deploy apps

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
@@ -86,7 +86,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 2
