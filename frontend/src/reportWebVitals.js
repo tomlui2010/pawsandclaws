29f406d58ef1c17c9ae731100f25d9fda39f8503
THOMAS LOUIS <tomlui2010@gmail.com>
2024-08-20 14:12:10 +1000

    added frontend-build job to github actions workflow

---
+++ frontend/src/reportWebVitals.js
@@ -0,0 +1,16 @@
+// reportWebVitals.js
+
+// The function to log web vitals
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry); // Cumulative Layout Shift
+      getFID(onPerfEntry); // First Input Delay
+      getFCP(onPerfEntry); // First Contentful Paint
+      getLCP(onPerfEntry); // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
+    });
+  }
+};
+
+export default reportWebVitals;
