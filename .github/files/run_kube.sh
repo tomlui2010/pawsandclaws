3622fa6fdea6ab474852880b531289144555a99f
THOMAS LOUIS <tomlui2010@gmail.com>
2024-08-21 14:03:24 +1000

    removed unwanted files

--- .github/files/run_kube.sh
+++
@@ -1,9 +0,0 @@
-    kubectl apply -f .circleci/files/deploy/backend-app-deployment.yml
-    kubectl apply -f .circleci/files/deploy/backend-app-service.yml
-    kubectl apply -f .circleci/files/deploy/templates-config.yml
-    
-    kubectl apply -f .circleci/files/deploy/frontend-app-deployment.yml
-    kubectl apply -f .circleci/files/deploy/frontend-app-service.yml
-    kubectl apply -f .circleci/files/deploy/nginx-configmap.yml
-
-    kubectl apply -f .circleci/files/deploy/postgres-deployment.yml
