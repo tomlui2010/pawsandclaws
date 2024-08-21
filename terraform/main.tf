480c1d34fe04588b7ca0d73ed3e44737b204086d
THOMAS LOUIS <tomlui2010@gmail.com>
2024-08-21 12:05:11 +1000

    moved cluster name to be fetched from github actions repo variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "pawsandclaws-eks-${random_string.suffix.result}"
+  cluster_name = "pawsandclaws"
 }
 
 resource "random_string" "suffix" {
